b561548177e446c4f1e299f69559bd98028b7566
user.email <luxuguangno1@163.com>
2026-09-13 15:42:57 +0800

    chore(release): bump VERSION to 0.1.1

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -16,7 +16,7 @@ import { scrubSecrets } from "@chita/agent/src/scrub.ts";
 import { runJudge } from "@chita/agent/src/judge.ts";
 import { printBanner } from "./banner.ts";
 
-export const VERSION = "0.1.0";
+export const VERSION = "0.1.1";
 
 function printVersion(): void {
   printBanner({ version: VERSION }, { short: true });
