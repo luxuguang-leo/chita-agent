64c66e753754446a8e83921cb81318a43d5b1444
user.email <luxuguangno1@163.com>
2026-09-19 17:35:43 +0800

    chore(release): bump VERSION to 0.1.2

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -17,7 +17,7 @@ import { scrubSecrets } from "@chita/agent/src/scrub.ts";
 import { runJudge } from "@chita/agent/src/judge.ts";
 import { printBanner } from "./banner.ts";
 
-export const VERSION = "0.1.1";
+export const VERSION = "0.1.2";
 
 function printVersion(): void {
   printBanner({ version: VERSION }, { short: true });
